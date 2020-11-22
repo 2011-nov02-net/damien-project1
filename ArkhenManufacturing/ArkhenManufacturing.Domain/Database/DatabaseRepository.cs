74c329ab7c33369423b4ca7da3825204a7c9e22e
Damien Bevins <damien.bevins@outlook.com>
2020-11-22 15:23:04 -0500

    Started the database repository, and created the project for testing it

--- ArkhenManufacturing/ArkhenManufacturing.Domain/Database/DatabaseRepository.cs
+++ ArkhenManufacturing/ArkhenManufacturing.Domain/Database/DatabaseRepository.cs
@@ -1,21 +1,39 @@
 ﻿using System;
 using System.Collections.Generic;
 
+using ArkhenManufacturing.DataAccess;
 using ArkhenManufacturing.Library;
 using ArkhenManufacturing.Library.Data;
 using ArkhenManufacturing.Library.Entity;
+
+using Microsoft.EntityFrameworkCore;
 
 namespace ArkhenManufacturing.Domain.Database
 {
     public class DatabaseRepository : IRepository
     {
-        // dbcontext
+        private readonly DbContextOptions<ArkhenManufacturingContext> _options;
 
         public DatabaseRepository(string connectionString) {
+            var optionsBuilder = new DbContextOptionsBuilder<ArkhenManufacturingContext>();
+            optionsBuilder.UseSqlServer(connectionString);
 
+            _options = optionsBuilder.Options;
         }
 
         public bool Any<T>() where T : ArkhEntity {
+            throw new NotImplementedException();
+        }
+
+        public bool Exists<T>(Guid id) where T : ArkhEntity {
+            throw new NotImplementedException();
+        }
+
+        public List<IData> RetrieveByName<T>(string name) where T : NamedArkhEntity {
+            throw new NotImplementedException();
+        }
+
+        public int Count<T>() where T : ArkhEntity {
             throw new NotImplementedException();
         }
 
@@ -27,19 +45,11 @@ namespace ArkhenManufacturing.Domain.Database
             throw new NotImplementedException();
         }
 
-        public bool Exists<T>(Guid id) where T : ArkhEntity {
-            throw new NotImplementedException();
-        }
-
-        public IData Retrieve<T>(Guid id) where T : ArkhEntity {
-            throw new NotImplementedException();
-        }
-
         public List<IData> RetrieveAll<T>() where T : ArkhEntity {
             throw new NotImplementedException();
         }
 
-        public List<IData> RetrieveByName<T>(string name) where T : NamedArkhEntity {
+        public IData Retrieve<T>(Guid id) where T : ArkhEntity {
             throw new NotImplementedException();
         }
 
