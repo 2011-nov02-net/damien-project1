3417273cf8eceba5aeb689e06447b7230017e988
Damien Bevins <damien.bevins@outlook.com>
2020-11-30 13:16:13 -0500

    Updated the regex patterns inside RegularExpressions.cs

--- ArkhenManufacturing/ArkhenManufacturing.WebApp/Misc/RegularExpressions.cs
+++ ArkhenManufacturing/ArkhenManufacturing.WebApp/Misc/RegularExpressions.cs
@@ -7,15 +7,15 @@ namespace ArkhenManufacturing.WebApp.Misc {
     public static class RegularExpressions 
     {
         public const string NumbersOnly = @"^[0-9]*$";
-        public const string NoSpecialCharacters = @"^[A-Z][A-Za-z0-9 '-,.#]*$";
-        public const string NameCharacters = @"^[A-Z][A-Za-z0-9 -]*$";
-        public const string UserNameCharacters = @"^[A-Z][A-Za-z0-9]*$";
+        public const string NoSpecialCharacters = @"^[A-Za-z0-9 '-,.#]*$";
+        public const string NameCharacters = @"^[A-Za-z0-9 -]*$";
+        public const string UserNameCharacters = @"^[A-Za-z0-9]*$";
         // TODO: Fill out the password characters regex
-        public const string PasswordCharacters = @"^[A-Za-z0-9!@#$%^&*-_+=]{8}+$";
+        public const string PasswordCharacters = @"^[A-Za-z0-9!@#$%&-_]{8,}$";
         // TODO: Fill out the email characters regex
-        public const string EmailCharacters = @"";
+        public const string EmailCharacters = @"^[a-zA-Z]+[a-zA-Z0-9\.]*\@[a-zA-Z]+[a-zA-Z0-9]+\.([a-zA-Z]+[a-zA-Z0-9]+){0,253}";
         // TODO: Fill out phone number characters regex
-        public const string PhoneNumber = @"";
+        public const string PhoneNumber = @"^(\+[0-9]{0,3})?[ -]?[0-9]{3}[ -]?[0-9]{3}[ -]?[0-9]{4}";
     }
 
 }
