af8757d527557ebfff72c159c8d182e629abf35f
Damien Bevins <damien.bevins@outlook.com>
2020-12-03 00:46:24 -0500

    Added view models for products being requested and a location version.

---
+++ ArkhenManufacturing/ArkhenManufacturing.WebApp/Models/LocationProductRequestViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ArkhenManufacturing.WebApp.Models
+{
+    public class LocationProductRequestViewModel
+    {
+        public Guid SelectedLocationId { get; set; }
+        public List<Tuple<string, Guid>> LocationNamesWithIds { get; set; }
+        public ProductRequestViewModel ProductRequestViewModel { get; set; }
+
+        public LocationProductRequestViewModel() { }
+    }
+}
---
+++ ArkhenManufacturing/ArkhenManufacturing.WebApp/Models/ProductRequestViewModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace ArkhenManufacturing.WebApp.Models
+{
+    public class ProductRequestViewModel
+    {
+        public Guid ProductId { get; set; }
+        [Range(1, 999999)]
+        public string ProductName { get; set; }
+        public int Count { get; set; }
+
+        public ProductRequestViewModel() { }
+
+        public ProductRequestViewModel(Guid productId) {
+            ProductId = productId;
+        }
+    }
+}
