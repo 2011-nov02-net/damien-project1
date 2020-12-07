bcd1d67fcf24a5d0745a5f1d1b328f6f55419091
Damien Bevins <damien.bevins@outlook.com>
2020-12-07 13:44:05 -0600

    Admins can now view all of the orders for a store (and subsequently go to their details).

--- ArkhenManufacturing/ArkhenManufacturing.WebApp/Models/LocationViewModel.cs
+++ ArkhenManufacturing/ArkhenManufacturing.WebApp/Models/LocationViewModel.cs
@@ -1,7 +1,10 @@
-﻿namespace ArkhenManufacturing.WebApp.Models
+﻿using System;
+
+namespace ArkhenManufacturing.WebApp.Models
 {
     public class LocationViewModel
     {
+        public Guid Id { get; set; }
         public string Name { get; set; }
 
         public LocationViewModel() { }
